140b4cb3d66352d8ed2f215d973949d6b111bc27
Yan <yg4886@utexas.edu>
2015-03-08 22:59:06 -0500

    python code tranform json to csv

---
+++ data/pdf_articles/mention_nltk_sentence/jsontocsv.py
@@ -0,0 +1,20 @@
+import json
+import csv
+import operator
+
+json_data = json.loads(open("mention_nltk_sentence_v2.json").read())
+
+
+csv_data=[]
+
+for (lineNO,value) in json_data.items():
+    for (m,n) in value["nltk_output_fragments"].items():
+        fragment=(str(value["article_id"]),str(value["mention_id"]),str(n["nltk_output_fragment"].encode('utf-8')),str(n["software"]))
+        csv_data.append(fragment)
+ 
+csv.writer(file('jsontocsv1.csv','w')) .writerows(csv_data)
+
+
+reader=csv.reader(open("jsontocsv1.csv"))
+sortedlist = sorted(reader, key=operator.itemgetter(1))
+csv.writer(file('jsontocsv_sorted1.csv','w')) .writerows(sortedlist)
